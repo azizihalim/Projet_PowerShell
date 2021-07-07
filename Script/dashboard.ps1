d416c8b95871550081db68e1a026a2407907a98a
Yochiro9 <85764388+Yochiro9@users.noreply.github.com>
2021-07-08 00:11:37 +0200

    Update dashboard.ps1

--- Script/dashboard.ps1
+++ Script/dashboard.ps1
@@ -1,5 +1,5 @@
-Install-Module Dashimo -Force
-Update-Module Dashimo
+#Install-Module Dashimo -Force
+#Update-Module Dashimo
 Import-Module D:\ESGI\Powershell\Monitoring\Projet_PowerShell\Script\monitoring.ps1
 Import-Module D:\ESGI\Powershell\Monitoring\Projet_PowerShell\Script\Script_Services_Process.ps1
 
@@ -8,36 +8,54 @@ $time = Get-Date
 $FirewallRule = Get-NetFirewallRule
 
 Dashboard -Name 'Monitoring' -FilePath $filepath  {
-    Tab -Name 'Accueil' {
+    TabOptions -SlimTabs -SelectorColor AntiqueWhite -Transition -LinearGradient -SelectorColorTarget DodgerBlue
+
+    Tab -IconSolid user-alt -IconColor Green -Name 'Accueil' {
         Section -Name 'Heure' {
-            Table -HideFooter -DataTable $time}
+            Panel {
+                Table -HideFooter -DataTable $time {
+                    TableConditionalFormatting -Name 'Name' -ComparisonType number -Operator ge -Value 'rezrzer' -BackgroundColor Green -row
                     }
+                }
+            }
+      }
+
     Tab -Name 'Hardware' {
         Section -Name 'Processeur : Charges - Vitesses'  {
-            Panel {
+            Panel{
                 Table -HideFooter -DataTable (charge_cpu) {
                     TableConditionalFormatting -Name 'Average' -ComparisonType number -Operator gt -Value 10 -Color White -BackgroundColor Crimson -Row
-            }
+                    }
             }
             Panel {
-                Table -HideFooter -DataTable (vitesse_cpu)
+                Table -HideFooter -DataTable (vitesse_cpu) {
+                    TableConditionalFormatting -Name 'currentclockspeed' -ComparisonType number -Operator lt -Value 3000 -Color White -BackgroundColor Green -Row
+                    TableConditionalFormatting -Name 'currentclockspeed' -ComparisonType number -Operator ge -Value 3000 -Color White -BackgroundColor Red -Row
+                    }
                 }
         }
         Section -Name 'Mémoire' {
-            Table -HideFooter -DataTable (RAM)
+            Table -HideFooter -DataTable (RAM) {
+                    TableConditionalFormatting -Name 'Name' -ComparisonType string -Operator contains -Value 'OK' -Color White -BackgroundColor Green -Row
+                    TableConditionalFormatting -Name 'Name' -ComparisonType string -Operator contains -Value 'WARNING' -Color White -BackgroundColor Orange -Row
+                    TableConditionalFormatting -Name 'Name' -ComparisonType string -Operator contains -Value 'CRITICAL' -Color White -BackgroundColor Crimson -Row
+                    }
             }
         Section -Name 'Informations du disque' {
             Panel{
                 Table -HideFooter -DataTable (total_DSize)
+                    
             }
             Panel {
-                Chart -Title 'Disque utilisé' -TitleAlignment center {}
-                Table -HideFooter -DataTable (total_DUsed) 
+                Table -HideFooter -DataTable (total_DUsed) {
+                    TableConditionalFormatting -Name 'Name' -ComparisonType string -Operator contains -Value 'OK' -Color White -BackgroundColor Green -Row
+                    TableConditionalFormatting -Name 'Name' -ComparisonType string -Operator contains -Value 'CRITICAL' -Color White -BackgroundColor Crimson -Row
+                    }
                 }           
             }
         }
     Tab -Name 'Software' {
-        Section -Name 'Nombre de services: total, running, stoppés' {
+        Section -Name 'Nombre de services: total, running, stoppés'{
             Panel {
                 Table -HideFooter -DataTable (total_services)
             }
@@ -50,14 +68,18 @@ Dashboard -Name 'Monitoring' -FilePath $filepath  {
         }
         Section -Name 'Liste des services' {
             Table -HideFooter -DataTable (liste_total_services) {
-                TableConditionalFormatting -Name 'Status' -ComparisonType string -Operator eq -Value 'Stopped' -Color BlueViolet -Row
+                TableConditionalFormatting -Name 'Status' -ComparisonType string -Operator eq -Value 'Stopped' -Color Crimson -Row
+                TableConditionalFormatting -Name 'Status' -ComparisonType string -Operator eq -Value 'Running' -Color BlueViolet -Row
             }
             }
         Section -Name 'Règles de Firewall' {
            Table -HideFooter -DataTable (rules_status)
             }
         Section -Name 'Liste des règles Firewall' {
-            Table -HideFooter -DataTable $FirewallRule
+            Table -HideFooter -DataTable $FirewallRule -PagingOptions @(5){
+                TableConditionalFormatting -Name 'Enabled' -ComparisonType string -Operator eq -Value 'False' -Color Crimson -Row
+                TableConditionalFormatting -Name 'Enabled' -ComparisonType string -Operator eq -Value 'True' -Color BlueViolet -Row
+                }
             }
     }
 }
